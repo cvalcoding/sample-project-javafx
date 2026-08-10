372e1210cf4b52de24beac56d55e8f60bdfb2702
cvalcy <cedric-valcy@hotmail.fr>
2026-08-10 23:04:17 +0200

    Début de la partie log

--- src/main/java/com/cvalcoding/sample_project_javafx/App.java
+++ src/main/java/com/cvalcoding/sample_project_javafx/App.java
@@ -1,57 +1,16 @@
 package com.cvalcoding.sample_project_javafx;
 
-import com.cvalcoding.sample_project_javafx.model.dao.EmployeeDAOImpl;
-import com.cvalcoding.sample_project_javafx.ui.Launcher;
-import javafx.application.Application;
+import com.cvalcoding.sample_project_javafx.config.LogManager;
 
 import java.sql.SQLException;
 
 public class App {
-	static final String QUERY = "SELECT * FROM employees";
+
 
 	public static void main(String[] args) throws SQLException {
-		
-		Application.launch(Launcher.class, args);
-		EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
-
-		/* Insert */
-//		Employee employee = new Employee();
-//		employee.setEmployeeNumber(1200);
-//		employee.setLastName("dupond");
-//		employee.setFirstName("paul");
-//		employee.setExtension("x302");
-//		employee.setEmail("paul.dupond@dev.com");
-//		employee.setOfficeCode("2");
-//		employee.setReportsTo(1102);
-//		employee.setJobTitle("Sales Rep");
-//
-//		employeeDAO.add(employee);
-
-
-//		List<Employee> employeeList = employeeDAO.getAll();
-//
-//		Employee employee = employeeList.stream().filter(emp -> emp.getEmployeeNumber() == 1200).findAny().orElse(null);
-//		Employee employee = employeeDAO.getAll().stream().filter(emp -> "1200".equals(emp.getEmployeeNumber())).findAny().orElse(null);
-
-//		for (Employee employee : employeeDAO.getAll()) {
-//			System.out.println(employee.getEmployeeNumber());
-//		}
-
-
-
-		/* Get */
-//		Employee employee = employeeDAO.get(1200);
-
-		/* Update */
-//		employee.setFirstName("toto");
-//		employee.setLastName("titita");
-//		employeeDAO.update(employee);
-
-//		System.out.println(employee.getLastName());
-//		System.out.println(employee.getFirstName());
-
-		/* Remove */
-//		employeeDAO.remove(employee);
+		LogManager.info("first test for info");
+//		Application.launch(Launcher.class, args);
+//		EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
 
 
 	}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.cvalcoding.sample_project_javafx {
 	requires javafx.controls;
 	requires static lombok;
 	requires java.sql;
+	requires org.apache.logging.log4j;
+	requires org.apache.logging.log4j.core;
 
 	exports com.cvalcoding.sample_project_javafx;
 	exports com.cvalcoding.sample_project_javafx.ui;
